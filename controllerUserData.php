2e091d286953fbfd544fd6c1609133cf84c73d49
DanteBisais <deep.dmb.bisais@gmail.com>
2022-07-04 14:41:02 +0800

    added add to card feature

--- controllerUserData.php
+++ controllerUserData.php
@@ -108,13 +108,46 @@ if (isset($_POST['login'])) {
         $fetch = mysqli_fetch_assoc($res);
         $fetch_pass = $fetch['user_password'];
         $fetch_access = $fetch['user_type'];
+        $uid = $fetch['user_id'];
         if (password_verify($password, $fetch_pass)) {
+
+            $row = mysqli_fetch_array($res);
+
+            $_SESSION['uid'] = 5;
+            $_SESSION['name'] = $fetch["first_name"];
+            $ip_add = getenv("REMOTE_ADDR");
+            if (isset($_COOKIE["product_list"])) {
+				$p_list = stripcslashes($_COOKIE["product_list"]);
+				//here we are decoding stored json product list cookie to normal array
+				$product_list = json_decode($p_list,true);
+				for ($i=0; $i < count($product_list); $i++) { 
+					//After getting user id from database here we are checking user cart item if there is already product is listed or not
+					$verify_cart = "SELECT id FROM cart WHERE user_id = $_SESSION[uid] AND p_id = ".$product_list[$i];
+					$result  = mysqli_query($con,$verify_cart);
+					if(mysqli_num_rows($result) < 1){
+						//if user is adding first time product into cart we will update user_id into database table with valid id
+						$update_cart = "UPDATE cart SET user_id = '$_SESSION[uid]' WHERE ip_add = '$ip_add' AND user_id = -1";
+						mysqli_query($con,$update_cart);
+					}else{
+						//if already that product is available into database table we will delete that record
+						$delete_existing_product = "DELETE FROM cart WHERE user_id = -1 AND ip_add = '$ip_add' AND p_id = ".$product_list[$i];
+						mysqli_query($con,$delete_existing_product);
+					}
+				}
+				//here we are destroying user cookie
+				setcookie("product_list","",strtotime("-1 day"),"/");
+				//if user is logging from after cart page we will send cart_login
+				echo "cart_login";
+				exit();	
+			}
+
             $_SESSION['email'] = $email;
             $status = $fetch['user_status'];
             if ($status == 'Active') {
                 $_SESSION['email'] = $email;
                 $_SESSION['password'] = $password;
                 $_SESSION['type'] = $fetch_access;
+                // $_SESSION['uid'] = $fetch_access;
                 header('location: index.php');
             } else {
                 $info = "It looks like you haven't still verify your email - $email";
